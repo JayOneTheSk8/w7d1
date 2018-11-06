2fd0c2730d39214ef7dbd5228919cac7ed60e5e3
Nathan Rice <nrice@oberlin.edu>
2018-11-06 15:10:22 -0500

    Create #fetchtodos

--- todo_app/frontend/todo_redux.jsx
+++ todo_app/frontend/todo_redux.jsx
@@ -4,10 +4,12 @@ import Store from './store/store';
 import { receiveTodo, receiveTodos } from './actions/todo_actions';
 import Root from './components/root';
 import { allTodos } from './reducers/selectors';
+import { fetchTodos } from './util/todo_api_util';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   window.store = Store(); //TEST
+  window.fetchTodos = fetchTodos;
   window.receiveTodo = receiveTodo; //TEST
   window.receiveTodos = receiveTodos; //TEST
   window.allTodos = allTodos; //TEST
